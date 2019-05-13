865a0499d42e799942ee43d4f3586e9b6a69d818
Joseph <josephryan96@gmail.com>
2019-05-13 02:58:33 -0700

    Move front-end to seperate repository

---
+++ src/App.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import './App.css';
+import {
+  Route
+} from "react-router-dom";
+import axios from 'axios'
+import Books from './components/Books'
+import BookForm from './components/BookForm'
+import NavBar from './components/NavBar'
+import BookView from './components/BookView'
+// import EditBook from './components/EditBook'
+import SearchBarContainer from './components/SearchBarContainer';
+
+const url = new URL('https://oer-bookr-api.herokuapp.com/')
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: []
+    }
+  }
+
+  componentDidMount() {
+
+    const books = JSON.parse(localStorage.getItem('books')) || []
+    this.setState({
+      books: books,
+      filteredBooks: books
+    })
+
+    axios
+      .get(`${url}books`)
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          books: res.data
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+  addBook = data => {
+    axios
+      .post(`${url}books`, data)
+      .then(res => {
+        console.log(res)
+        return axios
+          .get(`${url}books`)
+          .then(res => {
+            this.setState({
+              books: res.data
+            })
+          })
+      })
+      .catch(err => console.log(err))
+  }
+
+  deleteBook = id => {
+    axios
+      .delete(`${url}books/${id}`)
+      .then(res => {
+        console.log(res);
+        return axios
+          .get(`${url}books`)
+          .then(res => {
+            this.setState({
+              books: res.data
+            })
+          })
+          .catch(err => console.log(err))
+      })
+  }
+
+  searchBooks(query) {
+    let books = this.state.books.filter((book) => {
+      return book.title.toLowerCase().includes(query) || book.author.toLowerCase().includes(query) || book.subject.toLowerCase().includes(query)
+    });
+    this.setState({
+      books: books
+    })
+  }
+
+
+  render() {
+
+    return (
+      <div className="App">
+        <NavBar />
+        <Route
+          exact path='/'
+          render={props => <SearchBarContainer searchBooks={this.searchBooks.bind(this)} {...props} />}
+        />
+        <Route
+          exact path='/'
+          render={props => <Books books={this.state.books} {...props} />}
+        />
+        <Route path='/books/:id'
+          render={props => <BookView books={this.state.books} deleteBook={this.deleteBook} {...props} />}
+        />
+        <Route path='/create'
+          render={props => <BookForm addBook={this.addBook} {...props} />}
+        />
+        {/* <Route path='/books/:id'
+          render={props => <EditBook notes={this.state.books} {...props} />}
+        /> */}
+      </div>
+    );
+  }
+}
+
+
+export default App;
---
+++ src/components/BookForm.js
@@ -0,0 +1,126 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const StyledBookForm = styled.div`
+display: flex;
+flex-direction: column;
+width: 700px;
+display: flex;
+flex-wrap: wrap;
+justify-content: center;
+margin-left: 240px;
+h2 {
+    margin-right: 400px;
+}
+form {
+    margin-left: 50px;
+    display: flex;
+    flex-direction: column;
+}
+input {
+    box-sizing: border-box;
+    margin-bottom: 5px;
+    border: 2px solid #C9C9C9;
+    padding: 5px;
+    height: 50px;
+    :first-child {
+        // height: 50px;
+        padding-left: 10px;
+    }
+    :nth-child(2) {
+        // height: 500px;
+        width: 1000px;
+        text-align: top;
+        // padding-bottom: 440px;
+        padding-left: 10px;
+    }
+}
+button {
+    width: 210px;
+    height: 50px;
+    background-color: #433A30;
+    color: #F9FCFC;
+    border: 2px solid #C9C9C9;
+}
+`
+
+class BookForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            author: '',
+            publisher: '',
+            license: '',
+            subject: '',
+            image: '',
+            link: ''
+        };
+    }
+
+    submitChange = e => {
+        e.preventDefault();
+        this.props.addBook(this.state);
+        this.props.history.push("/");
+        // window.location.reload();
+    }
+
+    handleInputChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
+
+    render() {
+        return (
+            <StyledBookForm>
+                <h2>Add a Book:</h2>
+                <form onSubmit={this.submitChange}>
+                    <input
+                        onChange={this.handleInputChange}
+                        placeholder="Title"
+                        value={this.state.title}
+                        name="title"
+                    />
+                    <input
+                        onChange={this.handleInputChange}
+                        placeholder="Author"
+                        value={this.state.textBody}
+                        name="textBody"
+                    />
+                    <input
+                        onChange={this.handleInputChange}
+                        placeholder="Publisher"
+                        value={this.state.textBody}
+                        name="textBody"
+                    />
+                    <input
+                        onChange={this.handleInputChange}
+                        placeholder="License"
+                        value={this.state.textBody}
+                        name="textBody"
+                    />
+                    <input
+                        onChange={this.handleInputChange}
+                        placeholder="Subject"
+                        value={this.state.textBody}
+                        name="textBody"
+                    />
+                    <input
+                        onChange={this.handleInputChange}
+                        placeholder="Image"
+                        value={this.state.textBody}
+                        name="textBody"
+                    />
+                    <input
+                        onChange={this.handleInputChange}
+                        placeholder="Link"
+                        value={this.state.textBody}
+                        name="textBody"
+                    />
+                    <button type="submit">Save</button>
+                </form>
+            </StyledBookForm>
+        );
+    }
+}
+
+export default BookForm;
